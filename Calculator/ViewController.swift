f2639e76f5eb792f0d66fcccb85936e71312b204
Alexander Nelzin <asnelzin@gmail.com>
2015-05-14 22:07:30 +0300

    Add second and third lectures

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
     var userInTheMiddleOfTypingANumber: Bool = false
+    
+    var brain = CalcultorBrain()
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -21,6 +24,38 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             userInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userInTheMiddleOfTypingANumber = false
         }
     }
 }
